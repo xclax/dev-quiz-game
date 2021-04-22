0e4aa964f1dd50059b3ebdb3ccc15cf4f7ba42a1
clax <cl4x@protonmail.com>
2021-04-22 13:49:45 -0300

    creating the questions interface
    creating the questions interface and main screen

--- lib/widgets/score_card/score_card_widget.dart
+++ lib/widgets/score_card/score_card_widget.dart
@@ -9,34 +9,38 @@ class ScoreCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20),
+      padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
       child: Container(
         height: 136,
         decoration: BoxDecoration(
             color: AppColors.white, borderRadius: BorderRadius.circular(15)),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(flex: 1, child: ChartWidget()),
-            Expanded(
-                flex: 3,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 24),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Vamos Começar",
-                        style: AppTextStyles.heading,
-                      ),
-                      Text(
-                        "Complete os desafios e avance em conhecimento!",
-                        style: AppTextStyles.body,
-                      )
-                    ],
-                  ),
-                )),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(flex: 1, child: ChartWidget()),
+              Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 24, top: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Vamos Começar",
+                          style: AppTextStyles.heading,
+                        ),
+                        Text(
+                          "Complete os desafios e avance em conhecimento!",
+                          style: AppTextStyles.body,
+                        )
+                      ],
+                    ),
+                  )),
+            ],
+          ),
         ),
       ),
     );
